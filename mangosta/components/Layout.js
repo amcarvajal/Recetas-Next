c5c563babe4e555d7aa6095c23aff3a337c59af6
unknown <amcarvajal15@gmail.com>
2021-05-20 14:14:20 +0200

    First Commit

---
+++ mangosta/components/Layout.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <header>
+        <Link href="/">
+          <a>
+            <h1>
+              <span>Just Add</span>
+              <span>HomeMade</span>
+            </h1>
+            <h2>Delicious Food</h2>
+          </a>
+        </Link>
+      </header>
+
+      <div className="page-content">
+        { children }
+      </div>
+
+      <footer>
+        <p>Copyright 2021 Just-Add  :)</p>
+      </footer>
+    </div>
+  )
+}
